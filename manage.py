f2bccb683b6683a8e73a63fb3f9c0683cff476e5
Julien Lebunetel <julien@lebunetel.com>
2024-02-20 22:25:28 +0100

    feat(logging): set up logging

--- manage.py
+++ manage.py
@@ -4,10 +4,12 @@ import os
 import sys
 
 from django.core.management import execute_from_command_line
+from rich.traceback import install
 
 
 def main():
     """Run administrative tasks."""
+    install(width=80)
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "toolbox.settings")
     execute_from_command_line(sys.argv)
 

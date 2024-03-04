478f5d079b0c5b3769ce13812775e3abcf05879d
Julien Lebunetel <julien@lebunetel.com>
2024-03-04 18:24:46 +0100

    refactor(templates): move landing page to core app

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -2,11 +2,13 @@
 
 from django.templatetags.static import static
 from django.urls import URLPattern, URLResolver, path
+from django.views.generic import TemplateView
 from django.views.generic.base import RedirectView
 
 app_name: str = "core"
 
 urlpatterns: list[URLPattern | URLResolver] = [
+    path("", TemplateView.as_view(template_name="core/landing.html"), name="landing"),
     path(
         "favicon.svg",
         RedirectView.as_view(url=static("core/img/favicon.svg")),
--- toolbox/urls.py
+++ toolbox/urls.py
@@ -2,10 +2,8 @@
 
 from django.contrib import admin
 from django.urls import URLPattern, URLResolver, include, path
-from django.views.generic import TemplateView
 
 urlpatterns: list[URLPattern | URLResolver] = [
-    path("", TemplateView.as_view(template_name="landing.html"), name="landing"),
     path("", include("core.urls")),
     path("accounts/", include("accounts.urls")),
     path("accounts/", include("allauth.urls")),

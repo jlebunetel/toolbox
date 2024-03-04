dac467bb4ac7c112eed4e9d50d27a0c55b9f74bc
Julien Lebunetel <julien@lebunetel.com>
2024-03-04 17:11:24 +0100

    feat(frontend): add favicon utilities

--- toolbox/urls.py
+++ toolbox/urls.py
@@ -6,6 +6,7 @@ from django.views.generic import TemplateView
 
 urlpatterns: list[URLPattern | URLResolver] = [
     path("", TemplateView.as_view(template_name="landing.html"), name="landing"),
+    path("", include("core.urls")),
     path("accounts/", include("accounts.urls")),
     path("accounts/", include("allauth.urls")),
     path("admin/", admin.site.urls),

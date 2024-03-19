a9c2b9c62851dd021cd0989bc8fbf482fab68c38
Julien Lebunetel <julien@lebunetel.com>
2024-03-19 08:21:02 +0100

    feat(frontend): upcoming birthdays on the landing page

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,14 +1,14 @@
 """URL configuration for the 'core' application."""
 
+from core.views import LandingPageView
 from django.templatetags.static import static
 from django.urls import URLPattern, URLResolver, path
-from django.views.generic import TemplateView
 from django.views.generic.base import RedirectView
 
 app_name: str = "core"
 
 urlpatterns: list[URLPattern | URLResolver] = [
-    path("", TemplateView.as_view(template_name="core/landing.html"), name="landing"),
+    path("", LandingPageView.as_view(), name="landing"),
     path(
         "favicon.svg",
         RedirectView.as_view(url=static("core/img/favicon.svg")),
